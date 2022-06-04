bb4745566b3cdbd88e9482b869a8b32c4a707aef
Faheem Firdous <faheemfirdous77@gmail.com>
2022-06-04 10:39:49 +0530

    Create AddingTwoNumbers.c

---
+++ AddingTwoNumbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    float num1, num2, result;
+    printf("Welcome to the calculator.\nEnter first number:\n");
+    scanf("%f", &num1);
+    printf("Enter num2:\n");
+    scanf("%f", &num2);
+    result = num1 + num2;
+    printf("The sum of %f and %f is %f.",num1,num2,result);
+    return 0;
+}
