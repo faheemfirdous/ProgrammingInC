0d8c70684dabce1b26fc34ab38f295ab225d5396
Faheem Firdous <faheemfirdous77@gmail.com>
2022-06-04 10:41:13 +0530

    Create checkingOddEven.c

---
+++ checkingOddEven.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("Enter the number:\n");
+    scanf("%d",&a);
+    if (a%2==0)
+    {
+        printf("The number %d is even",a);
+    }
+    else
+    {
+        printf("The number %d is odd", a);
+    }
+  return 0;
+}
