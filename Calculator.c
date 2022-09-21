09c323e91151d0f8ee9a571947eb7833bb70970c
Faheem Firdous <faheemfirdous77@gmail.com>
2022-09-21 20:57:10 +0530

    Update Calculator.c

--- Calculator.c
+++ Calculator.c
@@ -6,48 +6,37 @@ int main()
     char operator;
     float result;
     printf("Welcome to the Faheem's Calculator!!!!\nSum-> +\nSubtract -> -\nMultiplication -> *\nDivide -> /n Modulus -> $\n");
-    printf("Enter first number:\n");
-    scanf("%d",&num1);
-    printf("Enter second number:\n");
-    scanf("%d",&num2);
+    printf("Enter 2 numbers:\n");
+    scanf("%d\n%d",&num1,&num2);
+    // printf("Enter second number:\n");
+    // scanf("%d",&num2);
     printf("What operation you want to perform:\n");
     scanf("%s",&operator);
     if (operator=='+') // sum
     {
         result=num1+num2;
-        printf("The result of %d and %d is %f.", num1, num2,result);
+        printf("The sum of %d and %d is %f.", num1, num2,result);
     }
-    else
-    {
-        if (operator=='-') // subtract
+    else if (operator=='-') // subtract
         {
             result=num1-num2;
-            printf("The result of %d and %d is %f.", num1, num2,result);
+            printf("The difference of %d and %d is %f.", num1, num2,result);
         }
-        else
+    else if (operator== '*') // multiplication
         {
-            if (operator== '*') // multiplication
-            {
-                result=num1*num2;
-                printf("The result of %d and %d is %f.", num1, num2,result);
-            }
-            else 
-            {
-                if (operator=='/') // divide
-                {
-                    result=num1/num2;
-                    printf("The result of %d and %d is %f.", num1, num2,result);
-                }
-                else
-                {
-                    if(operator=='$') // modulus
-                    {
-                        result=num1%num2;
-                        printf("The result of %d and %d is %f.", num1, num2,result);
-                    }
-                }
-            }
+            result=num1*num2;
+            printf("The multiplication of %d and %d is %f.", num1, num2,result);
         }
-    }
+    else if (operator=='/') // divide
+        {
+            result=num1/num2;
+            printf("The division of %d and %d is %f.", num1, num2,result);
+        }
+    else if(operator=='$') // modulus
+        {
+            result=num1%num2;
+            printf("The modulus of %d and %d is %f.", num1, num2,result);
+        }
+                
     return 0;
 }
