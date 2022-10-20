a5c90a44cd95c4011d4626dc9cb0baa2d9c17aeb
Faheem Firdous <faheemfirdous77@gmail.com>
2022-10-20 20:07:32 +0530

    Create salaraycalculator.c

---
+++ salaraycalculator.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    /*Ramesh’s basic salary is input through the keyboard. His dearness
+    allowance is 40% of basic salary, and house rent allowance is 20% of
+    basic salary. Write a program to calculate his gross salary.*/
+    printf("Enter the Basic salary:\n");
+    float GrossSalary;
+    int salary;
+    scanf("%d",&salary);
+    GrossSalary = salary + salary*0.6;
+    printf("The gross income is: %f", GrossSalary);
+    return 0;    
+}
